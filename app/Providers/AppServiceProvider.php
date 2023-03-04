ed1055a771f0488c7938ce01b1a96d458d0bd0d2
Stanislav Andrianov <sandrianov@tdera.ru>
2023-03-04 22:05:50 +0600

    Fix Kernel for whenRequestLifecycleIsLongerThan

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Carbon\CarbonInterval;
 use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Http\Kernel;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
@@ -28,17 +28,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //N+1 error
         Model::preventLazyLoading(!app()->isProduction());
+        //not set fillable property in model
         Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
-
-        //if script execution speed > 500 send notification in tg
+        // if db query > 500 ms
         DB::whenQueryingForLongerThan(500, function (Connection $connection) {
             logger()->channel('telegram')->debug('whenQueryingForLongerThan: ' . $connection->query()->toSql());
         });
-
+        // if execution time of script > 4 seconds
         $kernel = app(Kernel::class);
         $kernel->whenRequestLifecycleIsLongerThan(
-            CarbonInterval::seconds(4),
+            CarbonInterval::seconds(0.01),
             function () {
                 logger()
                     ->channel('telegram')
